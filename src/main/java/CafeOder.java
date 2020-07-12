f8be82b8b257fb9a756ca121150540f343b9b307
yhh1056 <yhh1056@naver.com>
2020-07-12 20:42:47 +0900

    프로젝트 시작

---
+++ src/main/java/CafeOder.java
@@ -0,0 +1,9 @@
+/**
+ * author {yhh1056}
+ * Create by {2020/07/12}
+ */
+public class CafeOder {
+    public void start() {
+
+    }
+}
---
+++ src/main/java/CafeOderApplication.java
@@ -0,0 +1,10 @@
+/**
+ * author {yhh1056}
+ * Create by {2020/07/12}
+ */
+public class CafeOderApplication {
+    public static void main(String[] args) {
+        CafeOder app = new CafeOder();
+        app.start();
+    }
+}
