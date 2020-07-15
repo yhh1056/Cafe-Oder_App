e25666b32b53dfaff3e09afd413f69ed414ad4b2
yhh1056 <yhh1056@naver.com>
2020-07-15 19:46:45 +0900

    메소드 네이밍 변경

--- src/main/java/view/CustomerMessage.java
+++ src/main/java/view/CustomerMessage.java
@@ -5,51 +5,51 @@ package view;
  * Create by {2020/07/13}
  */
 public class CustomerMessage {
-    public static void invalidChoiceMessage() {
+    public static void showInvalidChoice() {
         System.out.println("유효하지 않은 선택 입니다. 다시 선택해 주세요.");
     }
 
-    public static void modeListMessage() {
+    public static void showMode() {
         System.out.println( "1: 관리자 모드, 2: 사용자 모드, 3: 어플 종료");
     }
 
-    public static void successMessage() {
+    public static void showSuccess() {
         System.out.println("성공");
     }
 
-    public static void adminChoiceListMessage() {
+    public static void showAdminChoice() {
         System.out.println("1. 메뉴를 등록 합니다.");
         System.out.println("2. 메뉴를 삭제 합니다.");
         System.out.println("3. 매출을 확인 합니다.");
         System.out.println("4. 뒤로 갑니다.");
     }
 
-    public static void userChoiceListMessage() {
+    public static void showUserChoice() {
         System.out.println("1. 주문 합니다.");
         System.out.println("2. 뒤로 갑니다.");
     }
 
-    public static void adminRegisterMenuMessage() {
+    public static void showMenuRegister() {
         System.out.println("등록할 메뉴 이름과 가격을 입력해주세요. (엔터 기준)");
         System.out.println("이름은 15글자 이하로 가격은 100원 이상 100,000원 이하입니다.");
     }
 
-    public static void adminInvalidPriceMessage() {
+    public static void showInvalidPrice() {
         System.out.println("가격을 다시 입력해주세요.");
     }
 
-    public static void adminDeleteMenuMessage() {
+    public static void showDeleteMenu() {
         System.out.println("삭제할 메뉴 이름을 입력해주세요.");
     }
 
-    public static void userShowMenuListMessage() {
+    public static void showMenuList() {
         System.out.println("메뉴판 입니다.");
     }
-    public static void userChoiceMenuMessage() {
+    public static void showChoiceMenu() {
         System.out.println("메뉴를 선택해 주세요.");
     }
 
-    public static void userOrderSuccessMessage() {
+    public static void showOderSuccess() {
         System.out.println("주문이 완료 되었습니다. 맛있게 드세요.");
     }
 }
